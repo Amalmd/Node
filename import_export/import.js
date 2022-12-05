94e6c717c389ac55ab7e99a7200d882ef7d6de0c
Amal Medlege <amal@Amals-MacBook-Pro-2.local>
2022-12-06 00:02:06 +0200

    ex

---
+++ import_export/import.js
@@ -0,0 +1,6 @@
+//? Import / export
+
+// One of the major differences between require() and import()
+// is that require() can be called from anywhere inside the program
+// whereas import() cannot be called conditionally,
+// it always runs at the beginning of the file.
