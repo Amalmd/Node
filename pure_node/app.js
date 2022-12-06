7412aa2eec8698d060fa8119e888a2bd99dd4d4b
Amal Medlege <amal@Amals-MacBook-Pro-2.local>
2022-12-07 00:54:18 +0200

    pure node

---
+++ pure_node/app.js
@@ -0,0 +1,27 @@
+import http from "http";
+import { readFileSync } from "fs";
+
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+    if (req.url === "/") {
+      res.writeHead(200);
+      const data = readFileSync("index.html");
+      res.write(data);
+    } else if (req.url === "/raw-html") {
+      res.writeHead(200);
+      res.write("Welcome!");
+    } else if (req.url === "/users") {
+      res.writeHead(200);
+      const data = readFileSync("data.json");
+      res.write(data);
+    } else if (req.url === "/index.css") {
+      res.writeHead(200);
+      const data = readFileSync("index.css");
+      res.write(data);
+    }
+    res.end();
+  })
+  .listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
